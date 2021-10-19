b20eb74cb9954a45b0d5040a4e633fcc90dda83c
czewski <60816805+czewski@users.noreply.github.com>
2021-10-19 20:48:29 -0300

    lots of model testing and preprocessing
    still a lot of work to do but i think im getting somewhere xd

--- src/models.py
+++ src/models.py
@@ -3,18 +3,95 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import numpy as np 
-from pre_pro import X_train, X_test, PCA_X_train, PCA_X_test, y_train, y_test
-
 from sklearn.neural_network import MLPClassifier 
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score, mean_squared_error
+from sklearn.metrics import accuracy_score, mean_squared_error, mean_absolute_error
 
-from xgboost import XGBRegressor
+from xgboost import XGBClassifier
+
+from pre_pro import bb, X_train, X_test, y_train, y_test, X1_train, X1_test, y1_train, y1_test, X1_val, y1_val
 
 #%%
-#aplicar um modelo em diferentes conjuntos de treino, igual ta no slide. 
 
-#%%
+mlp=MLPClassifier(
+   hidden_layer_sizes=(100,1000,100),
+   activation="logistic",
+   solver='adam',
+   max_iter=10000, 
+   validation_fraction=0.1, 
+   #early_stopping=True,
+   learning_rate='constant', 
+   epsilon=1e-8,
+   verbose=True)
+#mlp.fit(X1_train,y1_train)
+mlp.fit(X_train,y_train)
+#print (mlp.score(X1_train,y1_train))
+print (mlp.score(X_test, y_test))
+plt.plot(mlp.loss_curve_)
+plt.plot(mlp.validation_scores_)
+
+
+#%% mlp com criterio de parada #antes separar em treino valid teste
+max_iter = 500
+mlp = MLPClassifier(hidden_layer_sizes=(1,16,1), activation='logistic', solver='adam', max_iter=max_iter)
+train_errors = []
+validation_errors = []
+for i in range(max_iter):
+     mlp.partial_fit(X1_train, y1_train, classes=np.unique(y1_train))
+     y_predict = mlp.predict(X1_train) #test maybe train here
+     y_predicted = mlp.predict(X1_val) #validação
+     
+     train_error = mean_absolute_error(y1_train, y_predict)
+     train_errors.append(train_error) #test
+
+     validation_error = mean_absolute_error(y1_val, y_predicted)
+     validation_errors.append(validation_error) #validação
+     
+     if np.remainder(i+1,100)==0:
+        print(((i+1)*100)/max_iter)
+        
+     if abs(train_error-validation_error)>=.1:
+         break
+     
+#%%     
+#print(mlp.score(X1_train,y1_train))
+print(mlp.score(X1_train,y1_train)) #score do treino
+print(mlp.score(X1_test, y1_test)) #score da validação
+
+#figures
+fig, ax = plt.subplots()
+ax.plot(train_errors, lw=1)
+ax.plot(validation_errors, lw=1)
+ax.set_xlabel('Epoch')
+ax.set_ylabel('Error')
+plt.title('Treino x Validação')
+plt.show()
+
+# fig, ax = plt.subplots()
+# ax.plot(X1_test,y_predict,'o-', lw=1)
+# #ax.plot(M[:,0], M[:,2], 'o--', lw=1)
+# ax.set_xlabel('Entrada')
+# ax.set_ylabel('Saída')
+# plt.title('Treino')
+# plt.show()
+
+
+# X_test=X_test.reshape(-1)
+# y_test=y_test.reshape(-1)
+# V = np.matrix([X_test,y_test,y_predicted]).transpose()
+# V.sort(axis=0, kind=None, order=None)
+# fig, ax = plt.subplots()
+# ax.plot(V[:,0],V[:,1],'o-', lw=1)
+# ax.plot(V[:,0], V[:,2], 'o--', lw=1) #predicted
+# ax.set_xlabel('Entrada')
+# ax.set_ylabel('Saída')
+# plt.title('Validação')
+# plt.show()
+
+# X_test=X_test.reshape(-1,1)
+# y_test=y_test.reshape(-1,1)
+
+#%% BASIC MLP CLASSIFIER
 # define and train an MLPClassifier named mlp on the given data
 mlp = MLPClassifier(hidden_layer_sizes=(50,200,50), max_iter=300, activation='relu', solver='adam', random_state=1)
 mlp.fit(PCA_X_train, y_train)
@@ -43,7 +120,7 @@ print(accuracy_score(y_test, predict))
 print("MSE: %f" % mean_squared_error(y_test, predict))
 
 # %% xgboost
-model = XGBRegressor()
+model = XGBClassifier()
 model.fit(PCA_X_train, y_train, verbose=False)
 predictions = model.predict(PCA_X_test)
 print("Erro Médio Absoluto: {:.2f}".format(mean_squared_error(predictions, y_test)))
--- src/pre_pro.py
+++ src/pre_pro.py
@@ -12,35 +12,50 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
 from xgboost import XGBRegressor
+from imblearn.over_sampling import SMOTE
 
+#%% MODELOS
 #xg boost
 #tem que usar mlp né por causa da materia feedforward
+  #nesse caso posso aplicar a validação durante o treino da rede
+    #validação cruzada tb
 #ainda preciso verificar treino igual foi feito no exercicio da outra materia
+
+#aplicar um modelo em diferentes conjuntos de treino, igual ta no slide. 
+#ver quais modelos de mlp, arquitetura da rede tb
+
+#entre 0 e 1 usar logistico e nao hiperbolico
+#mesmo com logistico, normalizar com medio 1 e 0
+
+#ver a heuristica do numero de camadas intermediarios
+
+#usar o treinador de hyperparemtros gridsearchCV
+
 #começar escrever artigo também. ... ..
+
+
+#%% PRE PROCESSAMENTO
+#pq dropei alguns valores (duration), se basear em correlação 
+    #remove highly correlated info; cause they mean the same
+
+#transformar categorica em hot encode e normalizar númerica
+
+#handle outliers# 
+# #.90 
+
+#explicar balanceamento, analisar pq to usando
 
 #%%
 df = pd.read_csv('../data/bank.csv', sep=';')
 
 # %% PRÉ-PROCESSAMENTO DE DADOS ===================================================
 
-# %% transformar categorica em hot encode e normalizar númerica
 
-#handle outliers# 
-# #.90 
-
-#remove highly correlated info; cause they mean the same
-
-#normalize data
-
-#balancear ainda, usar smote
-
-#aplicar ao outro dataset, o mais completo
-
-# %% encode output
+# %% encode output pra transformar yes/no pra 1/0
 df['y'] = LabelEncoder().fit_transform(df['y'])
 df['y']
 
-# %% checking the values in education field
+# %% checking the values in education field troca pra -1/1/2/3
 df['education'].value_counts()
 
 education_mapper = {"unknown":-1, "primary":1, "secondary":2, "tertiary":3}
@@ -75,21 +90,41 @@ bin_dict = {'yes':1, 'no':0}
 for item in binary_valued_features:
     df.replace({item:bin_dict},inplace=True)
 
-
+df.head()
 # %%rearrange the columns in the dataset to contain the y (target/label) at the end
 cols = list(df.columns.values)
 cols.pop(cols.index('y')) # pop y out of the list
 df = df[cols+['y']] #Create new dataframe with columns in new order
 df.describe()
 
-
 #%%split data
 y = df['y']
 X = df.values[:, :-1] # get all columns except the last column
 
-# spliting training and testing data
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=50)
+#smote
+sm = SMOTE(random_state=2)
+x_train_smo1, y_train_smo1 = sm.fit_resample(X, y.ravel())
 
+#%%  checking SMOTE progress
+print('no SMOTE')
+print(df['y'].value_counts())
+
+value = list(y_train_smo1)
+print('SMOTE')
+print('0', value.count(0))
+print('1', value.count(1))
+
+
+#%%
+
+# spliting training and testing data #
+X_train, X_test, y_train, y_test = train_test_split(x_train_smo1,y_train_smo1, test_size= 0.2, random_state=50)
+
+#splitting in training, test and validation; 80 10 10 
+X1_train, X1_test, y1_train, y1_test = train_test_split(x_train_smo1,y_train_smo1, test_size=0.1, random_state=1)
+
+X1_train, X1_val, y1_train, y1_val = train_test_split(X1_train, y1_train, test_size=0.125, random_state=1) # 0.25 x 0.8 = 0.2
+#x_train_smo, X_val, y_train_smo, Y_val = train_test_split(x_train_smo1,y_train_smo1, test_size= 0.3, random_state=0)
 
 #%%
 # Feature scaling #normalazing?
@@ -103,17 +138,21 @@ X_test = scaler.transform(X_test)
 X_train = pd.DataFrame(X_train)
 X_test = pd.DataFrame(X_test)
 
+print('end')
+
 #%% pca?
+#ctrl barra group comment
 
-from sklearn.decomposition import PCA
+# from sklearn.decomposition import PCA
 
-# apply the PCA for feature for feature reduction
-pca = PCA(n_components=0.95)
-pca.fit(X_train)
-PCA_X_train = pca.transform(X_train)
-PCA_X_test = pca.transform(X_test)
+# # apply the PCA for feature reduction 
+# #DONT THINK I NEED THIS 
+# pca = PCA(n_components=0.95)
+# pca.fit(X_train)
+# PCA_X_train = pca.transform(X_train)
+# PCA_X_test = pca.transform(X_test)
 
-X_train
+# X_train
 
 
 # %%
